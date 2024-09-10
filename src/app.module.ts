cb54ddc3ec8c4673302c362a0c6c134e3e614eab
venkateshl-wal <venkatesh.l@veltris.com>
2024-09-10 11:41:55 +0530

    nodemailer feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,24 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import * as mongoose from 'mongoose';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
     MongooseModule.forRoot(process.env.DB_URL),
+    MailerModule.forRoot(
+      {
+        transport: {
+          host: process.env.EMAIL_HOST,
+          port: 465,
+          secure: true,
+          auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD
+          },
+        },
+      }),
     UserModule,
     AuthModule,
   ],
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,15 +2,16 @@ import { ConflictException, Injectable } from '@nestjs/common';
 import { User } from './schema/user.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose';
-import { UserModule } from './user.module';
 import * as bcrypt from 'bcrypt';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectModel(User.name) private userModel: mongoose.Model<User>,
+    private readonly mailerService: MailerService,
   ) {}
-  async createUser(payload): Promise<UserModule> {
+  async createUser(payload): Promise<User> {
     try {
       const existingUser = await this.userModel.find({
         email: payload.email,
@@ -19,7 +20,77 @@ export class UserService {
         throw new ConflictException('User is already exists. Please login');
       }
       payload.password = await bcrypt.hash(payload.password, 10);
-      return await this.userModel.create(payload);
+      const user = await this.userModel.create(payload);
+      await this.mailerService.sendMail({
+        from: 'venkateshlendugure1999@gmail.com',
+        to: payload.email,
+        subject: 'Welcome to the app!',
+        html: `<!DOCTYPE html>
+        <html>
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Welcome to MicroServicesApp</title>
+          <style>
+            body {
+              font-family: Arial, sans-serif;
+              background-color: #f4f4f4;
+              padding: 20px;
+            }
+            .container {
+              background-color: #fff;
+              padding: 20px;
+              border-radius: 8px;
+              max-width: 600px;
+              margin: 0 auto;
+              box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            }
+            h1 {
+              color: #333;
+            }
+            p {
+              color: #555;
+            }
+            .details {
+              margin-top: 20px;
+            }
+            .details p {
+              margin: 5px 0;
+            }
+            .footer {
+              margin-top: 30px;
+              font-size: 12px;
+              color: #999;
+            }
+          </style>
+        </head>
+        <body>
+        
+          <div class="container">
+            <h1>Welcome aboard, ${payload.username}!</h1>
+            <p>Your account has been successfully created. We’re excited to have you as part of the MicroServicesApp family!</p>
+        
+            <div class="details">
+              <p><strong>Your account details:</strong></p>
+              <p><strong>Username:</strong> ${payload.username}</p>
+              <p><strong>Email:</strong> ${payload.email}</p>
+            </div>
+        
+            <p>Log in now to explore your dashboard and start making the most of our features!</p>
+        
+            <p>Need help? Our support team is ready to assist you.Reach them at ${'support_team@gmail.com'}</p>
+        
+            <div class="footer">
+              <p>Cheers,</p>
+              <p>The MicroServicesApp Team</p>
+            </div>
+          </div>
+        
+        </body>
+        </html>
+        `
+      });
+      return user;
     } catch (error) {
       throw error;
     }
